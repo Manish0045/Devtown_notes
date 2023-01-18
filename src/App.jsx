66aacfa1e72f22b034d0ba8602db614a5e25526f
Manish Sahani <101914715+Manish0045@users.noreply.github.com>
2023-01-18 09:48:13 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "../public/styles.css";
+import Footer from "./components/Footer";
+import Heading from "./components/Heading";
+import Note from "./components/Note";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Heading />
+      <Note />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
---
+++ src/components/Note.jsx
@@ -0,0 +1,16 @@
+var React = require("react");
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Devtown</h1>
+      <p>
+        We at DevTown (CODETOWN EDTECH PRIVATE LIMITED) (A research and
+        education-based startup ), are a team with an aim to provide students
+        with a platform to research and make projects in the field of AI and ML.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
